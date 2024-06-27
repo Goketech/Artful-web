42ca5aa7e82d9873e449ac8354c15d8c2ebf061c
Modupe Akanni <modupe775@gmail.com>
2024-06-27 23:48:04 +0100

    add why section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Nav from "../components/Nav";
 import Hero from "../components/Hero";
 import Discover from "../components/Discover";
 import Join from "../components/Join";
+import Why from "../components/Why";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Discover />
       <Join />
+      <Why />
     </main>
   );
 }
---
+++ components/Why.jsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+
+const Why = () => {
+  return (
+    <div className="flex justify-between mt-20 font-patrick">
+      <div className='p-10 pr-4'>
+        <h2 className='text-3xl mb-12'>Why Join the Waitlist?</h2>
+        <div className='grid grid-cols-2 text-xl gap-10'>
+          <div className='bg-gray-200 max-w-64 p-5 rounded-xl'>
+            <p>Early Access</p>
+            <span>
+              Be among the first to discover and collect the finest artworks
+              before they're available to the general public.
+            </span>
+          </div>
+          <div className='bg-gray-200 max-w-64 p-5 rounded-xl'>
+            <p>Exclusive Perks</p>
+            <span>
+              Enjoy special discounts, early-bird promotions, and curated
+              recommendations as a waitlist member.
+            </span>
+          </div>
+          <div className='bg-gray-200 p-5 rounded-xl col-span-2'>
+            <p>VIP Treatment</p>
+            <span>
+              Receive personalized attention and white-glove service from our
+              dedicated art concierge team.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/4">
+        <Image
+          src="/why.jpg"
+          alt="Join the Waitlist"
+          width={800}
+          height={600}
+          className="h-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Why;

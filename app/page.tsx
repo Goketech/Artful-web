c492bbcc3aed20ad7f9fb5037409b43110739c0d
Modupe Akanni <modupe775@gmail.com>
2024-06-27 21:25:03 +0100

    add discover section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Nav from "../components/Nav";
 import Hero from "../components/Hero";
+import Discover from "../components/Discover";
 
 export default function Home() {
   return (
     <main>
       <Nav />
       <Hero />
+      <Discover />
     </main>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,12 +8,12 @@ const Hero = () => {
                     alt="image of an art piece"
                     src={'/art.jpg'}
                     // fill
-                    className="w-full h-1/2"
+                    className="w-full"
                     width={100}
                     height={100}
                 />
             </div>
-            <div className="w-1/2 p-20 font-patrick flex flex-col"> 
+            <div className="w-1/2 pt-60 p-20 font-patrick flex flex-col"> 
                 <h1 className="text-5xl mb-4">Welcome to our Art Marketplace</h1>
                 <p className='text-2xl mt-6'>Discover a curated collection of stunning artwork from talented artists around the world. Explore and collect the pieces that speak to you.</p>
             </div>

99d897ebedf25f3c06a2f5db348b7436af169dc9
Modupe Akanni <modupe775@gmail.com>
2024-06-28 16:55:39 +0100

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import What from "../components/What";
 import Future from "../components/Future";
 import Waitlist from "../components/Waitlist";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       <Future />
       <Waitlist />
       <Contact />
+      <Footer />
     </main>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import Image from "next/image"
+import { Fragment, useState, useEffect } from "react";
+
+const Footer = () => {
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        setYear(new Date().getFullYear());
+      }, []);
+
+    return (
+        <div className="text-white">
+            <div className="flex bg-[#18212E] p-20 justify-between">
+                <div className="flex gap-20">
+                    <h1 className='font-snell text-5xl pl-6'>Artful</h1>
+                    <ul className="font-sans text-lg font-bold flex gap-10 items-center">
+                        <li>About Us</li>
+                        <li>Privacy Policy</li>
+                        <li>Careers</li>
+                        <li>Blog</li>
+                    </ul>
+                </div>
+                <div className="flex gap-10">
+                    <Image  alt="facebook logo"
+                    src={'/FB.svg'}
+                    width={32}
+                    height={32}
+                    />
+                    <Image  alt="twitter logo"
+                    src={'/Twitter.svg'}
+                    width={32}
+                    height={32}
+                    />
+                    <Image  alt="instagram logo"
+                    src={'/IG.svg'}
+                    width={32}
+                    height={32}
+                    />
+                </div>
+            </div>
+            <div className="bg-[#000000] p-8 font-sans text-center">
+            <p>{`© ${year} Artful`}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
